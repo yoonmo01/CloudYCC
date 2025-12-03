7b650d970a509d7be0417d4fe0d272f8787ca765
CYS <ok032765@gmail.com>
2025-12-03 13:38:49 +0900

    메인수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from CloudYCC.backend.app.api.v1.api import api_router
+
+from app.api.v1.api import api_router
 
 app = FastAPI(title="CloudYCC Project")
 
-# 전체 라우터 등록
 app.include_router(api_router, prefix="/api/v1")
 
 @app.get("/")
